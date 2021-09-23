bd349bb9d150ca6818ca68205fbcec0d1bf49738
cymhellfire <cym-hellfire@qq.com>
2021-09-23 22:11:10 +0800

    Initial commit

---
+++ Source/DrawAndGuess/DrawAndGuessGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DrawAndGuessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAWANDGUESS_API ADrawAndGuessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
